99c06a677cf30f33b7ee63b04e18b41929ebdddf
Dr.Kaminski <Dr.Kaminski@982f8ab8-0432-0410-be24-bf296a59f75d>
2007-06-03 19:14:03 +0000

    Initial commit.

---
+++ dscribe/test-job/projects/testSourceExtractor1/initial/workdir/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public String sayHello() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new HelloWorld().sayHello());
+    }
+
+}
