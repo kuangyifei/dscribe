d200f470a16109992c1087131688b14926362b9d
Dr.Kaminski <Dr.Kaminski@982f8ab8-0432-0410-be24-bf296a59f75d>
2007-06-03 19:14:03 +0000

    Initial commit.

---
+++ trunk/dscribe/test-job/projects/testSourceExtractor1/initial/workdir/Blah.java
@@ -0,0 +1,14 @@
+package test.p1;
+
+public class Blah {
+	
+	private String name;
+	
+	public class Oops {
+		private static Oops singleton;
+		private Blah cousin;
+	}
+	
+	private Foo otherFoo;
+	
+}
---
+++ trunk/dscribe/test-job/projects/testSourceExtractor1/initial/workdir/Foo.java
@@ -0,0 +1,14 @@
+package test.p1;
+
+import java.util.*;
+import java.awt.List;
+
+public interface Foo {
+	
+	private Set myStuff;
+	private Blah.Oops innerThing;
+	
+	private int[] numbers;
+	private List[] lists;
+	
+}
