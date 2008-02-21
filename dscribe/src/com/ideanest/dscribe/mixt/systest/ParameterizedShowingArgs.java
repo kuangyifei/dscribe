069410b1daa582dbf20833f6c4a6653d24d05925
Dr.Kaminski <Dr.Kaminski@982f8ab8-0432-0410-be24-bf296a59f75d>
2008-02-21 06:24:44 +0000

    MIXT systests:  added "check mods" handling with permissive XML diffing and parametrized test dynamically from spec file directory.

---
+++ dscribe/src/com/ideanest/dscribe/mixt/systest/ParameterizedShowingArgs.java
@@ -0,0 +1,103 @@
+package com.ideanest.dscribe.mixt.systest;
+
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.internal.runners.*;
+import org.junit.runner.notification.RunNotifier;
+
+public class ParameterizedShowingArgs extends CompositeRunner {
+	static class TestClassRunnerForParameters extends JUnit4ClassRunner {
+		private final Object[] parameters;
+		private final Constructor<?> constructor;
+		private final String paramString;
+
+		TestClassRunnerForParameters(TestClass testClass, Object[] parameters) throws InitializationError {
+			super(testClass.getJavaClass());
+			this.parameters = parameters;
+			this.constructor = getOnlyConstructor();
+			this.paramString = Arrays.asList(parameters).toString();
+		}
+
+		@Override protected Object createTest() throws Exception {
+			return constructor.newInstance(parameters);
+		}
+		
+		@Override protected String getName() {
+			return paramString;
+		}
+		
+		@Override protected String testName(final Method method) {
+			return method.getName();
+		}
+
+		private Constructor<?> getOnlyConstructor() {
+			Constructor<?>[] constructors= getTestClass().getJavaClass().getConstructors();
+			Assert.assertEquals(1, constructors.length);
+			return constructors[0];
+		}
+		
+		@Override protected void validate() throws InitializationError {
+			// do nothing: validated before.
+		}
+		
+		@Override public void run(RunNotifier notifier) {
+			runMethods(notifier);
+		}
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.METHOD)
+	public static @interface Parameters {
+	}
+	
+	private final TestClass testClass;
+
+	public ParameterizedShowingArgs(Class<?> klass) throws Exception {
+		super(klass.getName());
+		this.testClass = new TestClass(klass);
+		
+		MethodValidator methodValidator = new MethodValidator(testClass);
+		methodValidator.validateStaticMethods();
+		methodValidator.validateInstanceMethods();
+		methodValidator.assertValid();
+		
+		for (final Object each : getParametersList()) {
+			if (each instanceof Object[]) {
+				add(new TestClassRunnerForParameters(testClass, (Object[])each));
+			} else {
+				throw new Exception(String.format("%s.%s() must return a Collection of arrays.", testClass.getName(), getParametersMethod().getName()));
+			}
+		}
+	}
+	
+	@Override public void run(final RunNotifier notifier) {
+		new ClassRoadie(notifier, testClass, getDescription(), new Runnable() {
+			public void run() {
+				runChildren(notifier);
+			}
+		}).runProtected();
+	}
+	
+	private Collection<?> getParametersList() throws IllegalAccessException, InvocationTargetException, Exception {
+		return (Collection<?>) getParametersMethod().invoke(null);
+	}
+	
+	private Method getParametersMethod() throws Exception {
+		List<Method> methods= testClass.getAnnotatedMethods(Parameters.class);
+		for (Method each : methods) {
+			int modifiers= each.getModifiers();
+			if (Modifier.isStatic(modifiers) && Modifier.isPublic(modifiers)) return each;
+		}
+		throw new Exception("No public static parameters method on class " + getName());
+	}
+
+	public static Collection<Object[]> eachOne(Object... params) {
+		List<Object[]> results= new ArrayList<Object[]>();
+		for (Object param : params) results.add(new Object[] { param });
+		return results;
+	}
+}
+
--- dscribe/src/com/ideanest/dscribe/mixt/systest/SystemTest.java
+++ dscribe/src/com/ideanest/dscribe/mixt/systest/SystemTest.java
@@ -1,24 +1,52 @@
 package com.ideanest.dscribe.mixt.systest;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.*;
 import java.text.ParseException;
+import java.util.*;
 import java.util.regex.*;
 
+import org.custommonkey.xmlunit.*;
 import org.exist.fluent.*;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.w3c.dom.*;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 import com.ideanest.dscribe.mixt.*;
+import com.ideanest.dscribe.mixt.systest.ParameterizedShowingArgs.Parameters;
 
-@DatabaseTestCase.ConfigFile("test/conf.xml")
+@DatabaseTestCase.ConfigFile("test/conf.xml") @RunWith(ParameterizedShowingArgs.class)
 public class SystemTest extends DatabaseTestCase {
 	
+	private static final File TEST_SPEC_DIR = new File("test/systest-specs");
+
+	@Parameters public static Collection<Object[]> findSpecFiles() {
+		List<Object[]> list = new ArrayList<Object[]>();
+		for (File file : TEST_SPEC_DIR.listFiles()) {
+			if (!file.isHidden()) list.add(new Object[] {stripExtension(file.getName()), file});
+		}
+		return list;
+	}
+	
+	private static String stripExtension(String filename) {
+		int k = filename.lastIndexOf('.');
+		return k > 0 ? filename.substring(0, k) : filename;
+	}
+	
 	private static final Pattern STAGE_RE = Pattern.compile("#### stage (\\d+)");
-	private static final Pattern INSTRUCTION_RE = Pattern.compile("## (set|check) (file|rules)(.*)");
+	private static final Pattern INSTRUCTION_RE = Pattern.compile("## (set|check) (file|rules|mods)(.*)");
 	
 	private Folder workspace, rulespace;
 	private Transformer transformer;
+	private int stage = -1;
+	private File specFile;
+	
+	public SystemTest(String name, File specFile) {
+		this.specFile = specFile;
+	}
 	
 	@Before public void setUp() {
 		workspace = db.createFolder("/workspace");
@@ -26,9 +54,8 @@ public class SystemTest extends DatabaseTestCase {
 		transformer = new Transformer(workspace, rulespace);
 	}
 	
-	@Test public void run() throws IOException, ParseException, RuleBaseException, TransformException, InterruptedException {
-		BufferedReader reader = new BufferedReader(new FileReader("test/systest-specs/simple.txt"));
-		int stage = -1;
+	@Test public void run() throws IOException, ParseException, RuleBaseException, TransformException, InterruptedException, SAXException {
+		BufferedReader reader = new BufferedReader(new FileReader(specFile));
 		String line = reader.readLine();
 		while (line != null) {
 			line = line.trim();
@@ -55,7 +82,7 @@ public class SystemTest extends DatabaseTestCase {
 					if ("file".equals(matcher.group(2))) {
 						line = doCheckFile(reader, line, matcher.group(3).trim());
 					} else if ("mods".equals(matcher.group(2))) {
-						// TODO: implement modStore checker
+						line = doCheckMods(reader);
 					} else {
 						throw new IOException("bad check argument: " + line);
 					}
@@ -65,9 +92,8 @@ public class SystemTest extends DatabaseTestCase {
 	}
 
 	private String doSetRules(BufferedReader reader) throws IOException, ParseException {
-		String line;
 		StringBuilder buf = new StringBuilder();
-		line = readUntilNextInstruction(reader, buf);
+		String line = readUntilNextInstruction(reader, buf);
 		Reader bufReader = new StringReader(buf.toString());
 		try {
 			rulespace.documents().load(Name.overwrite("rules"), CompactFormTranslator.compactToXml(bufReader));
@@ -97,6 +123,46 @@ public class SystemTest extends DatabaseTestCase {
 					workspace.query().single("deep-equal($_1, $_2)", actual.root(), expected.root()).booleanValue());
 		} finally {
 			expected.delete();
+		}
+		return line;
+	}
+
+	private String doCheckMods(BufferedReader reader) throws IOException, SAXException {
+		StringBuilder buf = new StringBuilder();
+		buf.append("<mods xmlns='" + Transformer.MOD_NS + "'>\n");
+		String line = readUntilNextInstruction(reader, buf);
+		buf.append("</mods>");
+		String expected = buf.toString();
+		String actual = db.getDocument(Transformer.recordsRootPath() + workspace.path() + "/mods").contentsAsString();
+		XMLUnit.setNormalizeWhitespace(true);
+		XMLUnit.setIgnoreAttributeOrder(true);
+		Diff diff = new Diff(expected, actual);
+		diff.overrideDifferenceListener(new DifferenceListener() {
+			public int differenceFound(Difference difference) {
+				if (difference.getId() == DifferenceEngine.ATTR_VALUE_ID && "*".equals(difference.getControlNodeDetail().getValue())) {
+					return DifferenceListener.RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
+				} else if (difference.getId() == DifferenceEngine.TEXT_VALUE_ID && "*".equals(difference.getControlNodeDetail().getValue())) {
+					return DifferenceListener.RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
+				} else {
+					return DifferenceListener.RETURN_ACCEPT_DIFFERENCE;
+				}
+			}
+			public void skippedComparison(Node control, Node test) {
+			}
+		});
+		diff.overrideElementQualifier(new ElementQualifier() {
+			public boolean qualifyForComparison(Element control, Element test) {
+				if (control.hasAttribute("xml:id")) {
+					return test.hasAttribute("xml:id") && control.getAttribute("xml:id").equals(test.getAttribute("xml:id"));
+				} else if (control.hasAttribute("stage")) {
+					return test.hasAttribute("stage") && control.getAttribute("stage").equals(test.getAttribute("stage"));
+				} else {
+					return true;
+				}
+			}
+		});
+		if (!diff.similar()) {
+			fail("Mods differ after stage " + stage + "\n--- Expected:\n" + expected + "\n\n--- Actual: \n" + actual + "\n");
 		}
 		return line;
 	}
